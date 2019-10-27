782d2b959be49f3c15a5f058373d1e585b081c84
Simon Ribaut <simon.ribaut@outlook.com>
2019-10-27 22:25:31 +0100

    last commit

--- src/main/webapp/ressources/accueil.js
+++ src/main/webapp/ressources/accueil.js
@@ -69,10 +69,23 @@ function displayReps(idTable) {
 			var showName = row.insertCell(-1);
 			showName.innerHTML = spectacles[i].nom;
 			var repDate = row.insertCell(-1);
-			repDate.innerHTML = reps.date;
+			repDate.innerHTML = reps[j].date;
 			var repDuration = row.insertCell(-1);
-			repDuration.innerHTML = reps.duree;
+			repDuration.innerHTML = reps[j].duree;
+			var reserver = row.insertCell(-1);
+			var spec = spectacles[i].nom;
+			reserver.innerHTML = "<button type = 'button' onClick='Reserver(\""+spec+"\")'>Réserver</button>";
+			var acheter = row.insertCell(-1);
+			acheter.innerHTML = "<button type = 'button' onClick='Acheter(\""+spec+"\")'>Acheter</button>";
 		}
 	}
+}
+
+function Reserver(spectacle){
+	window.location="/Theater/reserver?spectacle="+spectacle;
+}
+
+function Acheter(spectacle){
+	window.location="/Theater/acheter?spectacle="+spectacle;
 }
  
